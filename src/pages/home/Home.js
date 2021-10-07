4b6fc09e35fa4b70e128aa2cccc6589102ea973f
Ivan Romero <38643671+CIRR14@users.noreply.github.com>
2021-10-07 17:00:00 -0500

    Initial commit. Topbar done

---
+++ src/pages/home/Home.js
@@ -0,0 +1,7 @@
+// dont need this after react 17 since it imports automatically
+// import React from 'react'
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home() {
+  return <Topbar />;
+}
